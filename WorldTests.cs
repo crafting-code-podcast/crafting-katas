1415b9425396db4ef5b71cb42baca3ae7949e218
davidadsit <david.adsit@gmail.com>
2022-11-05 07:43:57 -0600

    tracking living cells

--- WorldTests.cs
+++ WorldTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
@@ -10,17 +12,69 @@ public class WorldTests
     {
         var world = new World();
         var newWorld = world.Tick();
-        
+
         Assert.That(newWorld, Is.Not.SameAs(world));
     }
-    
-    
+
+    [Test]
+    public void Adding_a_living_cell_increases_population()
+    {
+        var world = new World();
+        Assert.That(world.Population, Is.EqualTo(0));
+
+        world.SetCellStatus(0, 0, Status.Alive);
+
+        Assert.That(world.Population, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Adding_a_dead_cell_does_not_increase_population()
+    {
+        var world = new World();
+        Assert.That(world.Population, Is.EqualTo(0));
+
+        world.SetCellStatus(0, 0, Status.Dead);
+
+        Assert.That(world.Population, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Cell_default_state_is_Dead()
+    {
+        var world = new World();
+        Assert.That(world.GetCellStatus(0, 0), Is.EqualTo(Status.Dead));
+    }
+
+    [Test]
+    public void Cell_can_live()
+    {
+        var world = new World();
+
+        world.SetCellStatus(0, 0, Status.Alive);
+
+        Assert.That(world.GetCellStatus(0, 0), Is.EqualTo(Status.Alive));
+    }
 }
 
 public class World
 {
+    private readonly List<(int row, int column)> livingCells = new();
+
     public World Tick()
     {
         return new World();
+    }
+
+    public int Population => livingCells.Count;
+
+    public void SetCellStatus(int row, int column, Status status)
+    {
+        livingCells.Remove((row, column));
+        if (status == Status.Alive) livingCells.Add((row, column));
+    }
+
+    public Status GetCellStatus(int row, int column)
+    {
+        return livingCells.Any(x => x.row == row && x.column == column) ? Status.Alive : Status.Dead;
     }
 }
